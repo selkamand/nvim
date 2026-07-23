5da35f34f818913e0ac7ca0bfc132b11f65e0aad
selkamand <sam.elkamand@gmail.com>
2026-07-23 22:29:18 +1000

    feat: add rustdoc snippet

--- after/ftplugin/r.lua
+++ after/ftplugin/r.lua
@@ -1,5 +1,5 @@
 --------------------------------------------
--- Create R-Specific Configuartion 
+-- Create R-Specific Configuartion
 --------------------------------------------
 -- Configure tmux helper
 local tmux = require("config.tmux")
@@ -11,17 +11,21 @@ tmux.setup({
 })
 
 -- Send load-all to tmux
-vim.keymap.set({ 'n', 'v' }, "<leader>cL", function() tmux.send("devtools::load_all(); ") end, {desc = "devtools::load_all()", noremap = true, silent = true})
-vim.keymap.set({ 'n', 'v' }, "<leader>cT", function() tmux.send("devtools::test(); ") end, {desc = "devtools::test()", noremap = true, silent = true})
-vim.keymap.set({ 'n', 'v' }, "<leader>cC", function() tmux.send("devtools::check(); ") end, {desc = "devtools::check()", noremap = true, silent = true})
-vim.keymap.set({ 'n', 'v' }, "<leader>cD", function() tmux.send("devtools::document(); ") end, {desc = "devtools::document()", noremap = true, silent = true})
-vim.keymap.set({ 'n', 'v' }, "<leader>cB", function() tmux.send("devtools::build_readme(); ") end, {desc = "devtools::build_readme()", noremap = true, silent = true})
+vim.keymap.set({ 'n', 'v' }, "<leader>cL", function() tmux.send("devtools::load_all(); ") end,
+  { desc = "devtools::load_all()", noremap = true, silent = true })
+vim.keymap.set({ 'n', 'v' }, "<leader>cT", function() tmux.send("devtools::test(); ") end,
+  { desc = "devtools::test()", noremap = true, silent = true })
+vim.keymap.set({ 'n', 'v' }, "<leader>cC", function() tmux.send("devtools::check(); ") end,
+  { desc = "devtools::check()", noremap = true, silent = true })
+vim.keymap.set({ 'n', 'v' }, "<leader>cD", function() tmux.send("devtools::document(); ") end,
+  { desc = "devtools::document()", noremap = true, silent = true })
+vim.keymap.set({ 'n', 'v' }, "<leader>cB", function() tmux.send("devtools::build_readme(); ") end,
+  { desc = "devtools::build_readme()", noremap = true, silent = true })
 
--- Execute the entire script with Rscript 
+-- Execute the entire script with Rscript
 vim.keymap.set(
   { 'n' }, "<leader>cR",
   function()
     vim.cmd(utils.current_filepath_escaped())
   end,
-  {desc = "Rscript current file", noremap = true, silent = true})
-
+  { desc = "Rscript current file", noremap = true, silent = true })
